080fb26c52de4ee8d2eaa9a8b7cfcdf7da8d40e2
Tonii <133613491+takogames@users.noreply.github.com>
2024-10-12 00:14:07 +0000

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,11 +13,11 @@
 
   networking.hostName = "nixos"; # Define your hostname.
 
-  time.timeZone = "Asia/Tashkent"; # Set your time zone.
+  time.timeZone = "America/Chicago"; # Set your time zone.
 
   i18n.defaultLocale = "en_US.UTF-8"; # Select internationalisation properties.
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
+  # nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enabling flakes
 
   system.stateVersion = "23.05"; # Don't change it bro
 }
